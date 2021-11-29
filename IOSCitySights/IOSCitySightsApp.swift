39857d658fd9c2f44a8231cb8751b99d935cd4c3
JiwonHae <hjiwon1994@gmail.com>
2021-11-30 00:42:50 +0900

    Initial Commit

---
+++ IOSCitySights/IOSCitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSCitySightApp.swift
+//  IOSCitySight
+//
+//  Created by Jiwon_Hae on 2021/11/30.
+//
+
+import SwiftUI
+
+@main
+struct IOSCitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
