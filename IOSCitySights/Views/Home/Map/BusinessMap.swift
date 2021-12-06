325e8e62ec623e265e3bff562208a983b5c70f1b
JiwonHae <hjiwon1994@gmail.com>
2021-12-07 04:12:41 +0900

    FEAT : create coordinator to detect user click on map view to display information on the clicked item

--- IOSCitySights/Views/Home/Map/BusinessMap.swift
+++ IOSCitySights/Views/Home/Map/BusinessMap.swift
@@ -27,15 +27,14 @@ struct BusinessMap : UIViewRepresentable{
                 
                 annotations.append(a)
             }
-            
         }
         
         return annotations
-        
     }
     
     func makeUIView(context: Context) -> MKMapView {
         let mapView = MKMapView()
+        mapView.delegate = context.coordinator
         
         // Make the user show up on the map
         mapView.showsUserLocation = true
@@ -55,5 +54,38 @@ struct BusinessMap : UIViewRepresentable{
     
     static func dismantleUIView(_ uiView: MKMapView, coordinator: ()) {
         uiView.removeAnnotations(uiView.annotations)
+    }
+    
+    //MARK: - Coordinate class
+    func makeCoordinator() -> Coordinator {
+        return Coordinator()
+    }
+    
+    class Coordinator: NSObject, MKMapViewDelegate{
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            
+            // If the annotation is the user blue dot, return nil
+            if annotation is MKUserLocation{
+                return nil
+            }
+            
+            // Check if there's a resuable nanotation view first
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: Constants.annotationReuseID)
+            
+            if annotationView == nil{
+                
+                // Create an annotation
+                annotationView = MKMarkerAnnotationView(
+                    annotation: annotation, reuseIdentifier: Constants.annotationReuseID)
+                
+                annotationView!.canShowCallout = true
+                annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            } else{
+                annotationView!.annotation = annotation
+            }
+            
+            return annotationView
+            
+        }
     }
 }
